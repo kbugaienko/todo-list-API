39fc590ee521509508807f8aab5feb580715785a
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2018-12-05 13:24:32 +0100

    Add 'body-parser' for request body

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+    bodyParser = require("body-parser");
 
 var todoRoutes = require("./routes/todos");
 
+// this two lines allow us to accsess the request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.send("Hello From Root Route!");
