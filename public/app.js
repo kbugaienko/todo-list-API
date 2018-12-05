cd4213ccb8e23a613de913447106e3255857164f
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2018-12-05 17:24:53 +0100

    Writing an initial AJAX call

--- public/app.js
+++ public/app.js
@@ -1,1 +1,21 @@
-alert("Hi From App.JS");
+$(document).ready(function(){
+  $.getJSON('/api/todos')
+  .then(addTodos)
+});
+
+function addTodos(todos){
+  // add todos to the page here
+  todos.forEach(function(todo){
+    addTodo(todo);
+  });
+}
+
+function addTodo(todo){
+  var newTodo = $('<li class="task">'+todo.name +' <span>X</span></li>');
+  newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed);
+  if(todo.completed){
+    newTodo.addClass("done");
+  }
+  $('.list').append(newTodo);
+}
