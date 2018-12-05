7c0a2d02ee368fcc07466c028daa3138347ed8fe
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2018-12-05 16:39:56 +0100

    Create main page
    * create main page 'index.html'
    * create public folder with two files: 'app.css' and 'app.js'
    * install nodemon for automatic restart the server
    * command to install: 'npm install -g nodemon'

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ var todoRoutes = require("./routes/todos");
 // this two lines allow us to accsess the request body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+// say app where to search file "index.html"
+app.use(express.static(__dirname + "/views"));
 
 app.get("/", function(req, res){
-  res.send("Hello From Root Route!");
+  res.sendFile("index.html");
 });
 
 app.use("/api/todos/", todoRoutes);
