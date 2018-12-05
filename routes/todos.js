306fa0e42101b1cfbb6e59c8b2128fe5e38eb96b
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2018-12-05 13:36:14 +0100

    Create route for create new entry

--- routes/todos.js
+++ routes/todos.js
@@ -3,6 +3,7 @@ var router = express.Router();
 // connect to db via the folder "models" in file "index.js"
 var db = require("../models");
 
+// route to show all existing entries
 router.get("/", function(req, res){
   // method 'find' mongoose is connect to our database and find all todos
   db.Todo.find()
@@ -12,6 +13,17 @@ router.get("/", function(req, res){
   .catch(function(err){
     res.send(err);
   })
+});
+
+// route for create new entry
+router.post("/", function(req, res){
+  db.Todo.create(req.body)
+  .then(function(newTodo){
+    res.status(201).json(newTodo);
+  })
+  .catch(function(err){
+    res.send(err);
+  });
 });
 
 module.exports = router;
